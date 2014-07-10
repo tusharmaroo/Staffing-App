338daee768c49e0105102393fe2eaa5af7bc760b
tusharmaroo <tusharmaroo@gmail.com>
2014-07-11 00:21:09 +0530

    authentication changed

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => 'tusharmaroo',
-  :password             => '9414859022Tushar',
+  :user_name            => 'mydomain100',
+  :password             => '9414859022tushar',
   :authentication       => 'plain',
   :enable_starttls_auto => true
 }
