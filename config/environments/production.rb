c2d3ad6bc05f18e526f5524bacd193299822abf9
tusharmaroo <tusharmaroo@gmail.com>
2014-07-10 14:56:34 +0530

    modified mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,15 +45,17 @@ Rails.application.configure do
   # Set to :debug to see everything in the log.
   config.log_level = :info
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'stormy-ravine-9512.herokuapp.com',
-    user_name:            'vishalattcs',
-    password:             'mypasstcs',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'vishalattcs',
+  :password             => 'mypasstcs',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true
+}
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
